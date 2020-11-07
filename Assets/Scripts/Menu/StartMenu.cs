c0f5c1b004524bca8d806119161342fb9db2c6cc
Tobias Hendricks <tobias.hendricks@gmx.de>
2020-11-07 16:03:08 +0100

    Added Start Scene

---
+++ Assets/Scripts/Menu/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public SceneField Scene;
+    public SceneField BackScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(Scene.SceneName);
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(BackScene.SceneName);
+    }
+}
