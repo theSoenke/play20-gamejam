bc0269138f39b60d821c232fd5571a04c0939ab9
gurkenhobel <nilsw.agner@web.de>
2020-11-07 17:49:48 +0100

    integrated walk animation

---
+++ Assets/Scripts/KenVisualsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class KenVisualsManager : MonoBehaviour
+{
+    public Animator Animator;
+    public NavMeshAgent Nav;
+
+    [Range(0,1)]
+    public float AnimationSpeedFactor;
+
+    public void Update()
+    {
+        Animator.SetFloat("WalkSpeed", Nav.velocity.magnitude * AnimationSpeedFactor );
+        Animator.SetBool("IsWalking", Nav.enabled);
+    }
+}
