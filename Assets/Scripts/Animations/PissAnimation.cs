f4b94e2f405300c470c60b530d249f22e74d10a5
gurkenhobel <nilsw.agner@web.de>
2020-11-07 13:34:19 +0100

    imported house assets

---
+++ Assets/Scripts/Animations/PissAnimation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PissAnimation : ActionAnimation
+{
+    public override void RunAnimation(GameState state)
+    {
+        IsRunning = true;
+        
+    }
+}
