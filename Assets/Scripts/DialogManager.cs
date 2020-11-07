33ff885680660f5061950496044a113fffa502f8
theSoenke <theSoenke@users.noreply.github.com>
2020-11-07 10:51:33 +0100

    Add dialog manager

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public GameStateManager gameStateManager;
+    public Button[] buttons;
+
+    void Start()
+    {
+        var actions = gameStateManager.Actions;
+
+        if (actions.Length < buttons.Length)
+        {
+            Debug.LogError("Gimme some actions");
+            return;
+        }
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            var button = buttons[i];
+            var text = button.GetComponentInChildren<Text>();
+            text.text = actions[i].Description();
+        }
+    }
+}
