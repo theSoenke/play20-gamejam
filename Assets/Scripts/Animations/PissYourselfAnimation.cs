6a02e171a90277fe14b3533dfb0ffd182b9161f8
Tobias Hendricks <tobias.hendricks@gmx.de>
2020-11-08 00:11:17 +0100

    Add Animation  logic for peeyourself

--- Assets/Scripts/Animations/PissYourselfAnimation.cs
+++ Assets/Scripts/Animations/PissYourselfAnimation.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class PissYourselfAnimation : ActionAnimation
 {
+    private AudioClip _musicClip;
+
     public AudioSource MusicAudioSource;
     public AudioSource KenAudio;
     public AudioClip PeedYourselfClip;
     public AudioClip ScratchClip;
 
+    private void Awake()
+    {
+        _musicClip = MusicAudioSource.clip;
+    }
+
     private void Update()
     {
         if (IsRunning)
         {
-            IsRunning = false;
+            if (!KenAudio.isPlaying)
+            {
+                MusicAudioSource.clip = _musicClip;
+                MusicAudioSource.Play();
+                IsRunning = false;
+            }
         }
     }
 
     public override void RunAnimation(GameState state)
     {
         IsRunning = true;
+        MusicAudioSource.clip = ScratchClip;
+        MusicAudioSource.Play();
+        KenAudio.clip = PeedYourselfClip;
+        KenAudio.Play();
     }      
 }
