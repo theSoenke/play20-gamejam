f68a45ebad02b8e4daf385a87e73edc44caf794f
Tobias Hendricks <tobias.hendricks@gmx.de>
2020-11-07 23:46:41 +0100

    Add Peedyourself

---
+++ Assets/Scripts/Actions/PissYourselfAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PissYourselfAction : ActionDescription 
+{
+    public float SusValue = 50f;
+
+    public float MinPeeLevel = 50;
+    public float PeeLevelFactor = 0.5f;
+
+    public override string Description(GameState state)
+    {
+        return "Piss yourself.";
+    }
+
+    public override int EvaluateProbability(GameState state)
+    {
+        if (!state.IsInside || state.PeeLevel < MinPeeLevel)
+        {
+            return 0;
+        }
+        return Mathf.RoundToInt(state.PeeLevel * PeeLevelFactor);
+    }
+
+    public override void Execute(GameState state)
+    {
+        state.SusAdd(SusValue);
+        state.PissReset(true);
+    }    
+}
---
+++ Assets/Scripts/Animations/PissYourselfAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PissYourselfAnimation : ActionAnimation
+{
+
+    private void Update()
+    {
+        if (IsRunning)
+        {
+            IsRunning = false;
+        }
+    }
+
+    public override void RunAnimation(GameState state)
+    {
+        IsRunning = true;
+    }      
+}
