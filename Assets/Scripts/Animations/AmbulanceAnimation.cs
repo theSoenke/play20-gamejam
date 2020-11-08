e44c091fba07f2135d0f83e953f20b9e3a7e85ce
gurkenhobel <nilsw.agner@web.de>
2020-11-08 13:11:56 +0100

    added ambulance animation

---
+++ Assets/Scripts/Animations/AmbulanceAnimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AmbulanceAnimation : ActionAnimation
+{
+    public GameObject Ambulance;
+    public Transform KenAmbulanceTransform;
+    public NavMeshAgent Ken;
+    public AudioSource KenAudio;
+    public AudioClip AmbulanceClip;
+    public Transform DoorPosition;
+    public float AnimationDuration;
+
+
+    private Vector3 _ambulanceStart;
+    private float _startTime;
+
+
+    public override void RunAnimation(GameState state)
+    {
+        IsRunning = true;
+        KenAudio.clip = AmbulanceClip;
+        KenAudio.Play();
+        Ken.enabled = false;
+        Ambulance.SetActive(true);
+        Ken.transform.parent = KenAmbulanceTransform;
+        Ken.transform.localPosition = Vector3.zero;
+        Ken.transform.localRotation = Quaternion.identity;
+        _ambulanceStart = Ambulance.transform.position;
+        _startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsRunning)
+        {
+            var animationProgress = (Time.time - _startTime) / AnimationDuration;
+            Ambulance.transform.position = Vector3.Lerp(_ambulanceStart, DoorPosition.position, animationProgress);
+            if(Vector3.Distance(DoorPosition.position, Ambulance.transform.position) < 0.3) {
+                Ambulance.SetActive(false);
+                KenAudio.Stop();
+                IsRunning = false;
+            }
+        }
+    }
+}
+
